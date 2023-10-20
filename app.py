5c260791c31fedfe9fe175fe7a9a77819aa92761
SebastianPineda <88106476+jspinedas@users.noreply.github.com>
2023-10-20 15:56:00 -0500

    base task detele y get

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 def connect_to_database_with_retry(db_uri, max_retries=5, retry_interval=5):
-    retries = 0
-    while retries < max_retries:
-        try:
-            db.init_app(app)
-            with app.app_context():
-                db.create_all()
-                return
-        except OperationalError:
-            # Connection failed, wait and then retry
-            time.sleep(retry_interval)
-            retries += 1
+  retries = 0
+  while retries < max_retries:
+    try:
+      db.init_app(app)
+      with app.app_context():
+        db.create_all()
+        return
+    except OperationalError:
+      # Connection failed, wait and then retry
+      time.sleep(retry_interval)
+      retries += 1
 
-    # If max retries reached without a successful connection, raise an error
-    raise Exception("Failed to connect to the database.")
+  # If max retries reached without a successful connection, raise an error
+  raise Exception("Failed to connect to the database.")
 
 connect_to_database_with_retry(app.config['SQLALCHEMY_DATABASE_URI'])
 
@@ -35,4 +35,5 @@ api = Api(app)
 api.add_resource(VistaSignUp, '/api/auth/signup')
 api.add_resource(VistaLogin, '/api/auth/login')
 api.add_resource(VistaArchivo, '/api/tasks')
+api.add_resource(VistaArchivo, '/api/tasks/<int:id_task>')
 jwt = JWTManager(app)
