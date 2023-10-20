80a0b17d71cfaba062ad897409d794aac82f44d7
Daniel Henao <daniel.henao@geniussports.com>
2023-10-19 19:45:45 -0500

    Nuevo puerto para bd

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
-#from flask_cors import CORS
 #from flask_jwt_extended import JWTManager
-#from flask_restful import Api
+from flask_restful import Api
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = '' #Connection postgres
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+app_context = app.app_context()
+app_context.push()
+
+api = Api(app)
+api.add_resource(VistaSignIn, '/signin')
 
 @app.route("/")
 def helloWord():
